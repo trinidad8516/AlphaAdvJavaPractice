c097469a089469e81e49b34626a21ca3ae40e2fc
Trinidad <trinidad8516@gmail.com>
2014-03-08 02:06:18 -0500

    First commit to Repository.

---
+++ SamplesOfJavaBookLiangCITP290/src/chapter15/Edible.java
@@ -0,0 +1,6 @@
+package chapter15;
+
+public interface Edible {
+  /** Describe how to eat */
+  public abstract String howToEat();
+}
---
+++ SamplesOfJavaBookLiangCITP290/src/chapter40/MyImageCellRenderer.java
@@ -0,0 +1,21 @@
+package chapter40;
+
+
+import javax.swing.*;
+import javax.swing.table.*;
+
+import chapter13.ImageViewer;
+
+import java.awt.*;
+
+public class MyImageCellRenderer extends DefaultTableCellRenderer {
+  /** Override this method in DefaultTableCellRenderer */
+  public Component getTableCellRendererComponent
+      (JTable table, Object value, boolean isSelected,
+       boolean isFocused, int row, int column) {
+    Image image = ((ImageIcon)value).getImage();
+    ImageViewer imageViewer = new ImageViewer(image);
+
+    return imageViewer;
+  }
+}
